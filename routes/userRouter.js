c377cc05bbf06d6a6c5f4928d91ffd87cb1c9e5f
SauravKumar1276 <kumarsaurav11494@gmail.com>
2024-05-27 20:47:17 +0530

    route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,6 +10,12 @@ import { isAuthenticated } from "../middleware/auth.js";
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
+router.get("/view", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "get",
+  });
+});
 router.get("/logout", isAuthenticated, logOut);
 router.get("/myProfile", isAuthenticated, getMyProfile);
 router.get("/getAuthors", getAuthors);
