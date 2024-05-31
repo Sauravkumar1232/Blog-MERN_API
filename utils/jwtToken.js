0a4868e4ad8bd20d0c4e70e8620bf0f72e2c1a4b
Sauravkumar1232 <97805415+Sauravkumar1232@users.noreply.github.com>
2024-05-31 10:19:07 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,6 +1,6 @@
 export const sendToken = (user, statusCode, message, res) => {
   const token = user.getJWTToken();
-  // console.log(token);
+  console.log(token, "token from jwtToken");
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
