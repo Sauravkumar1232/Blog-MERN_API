dbb2c2fcb7d546a023a731ac5f5bdf6784221e6b
Sauravkumar1232 <97805415+Sauravkumar1232@users.noreply.github.com>
2024-06-02 16:21:46 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,15 +1,20 @@
 export const sendToken = (user, statusCode, message, res) => {
   const token = user.getJWTToken();
   console.log(token, "token from jwtToken");
-  const options = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
-    res.cookie("authcookie", token, { maxAge: 9000000, httpOnly: true });
-
-  res.status(statusCode).cookie("token", token, options).json({
+  // const options = {
+  //   expires: new Date(
+  //     Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+  //   ),
+  //   httpOnly: true,
+  // };
+    // res.cookie("authcookie", token, { maxAge: 9000000, httpOnly: true });
+ res.cookie('token', token, {
+        httpOnly: true,        // Accessible only by web server
+        secure: true,          // Use HTTPS
+        sameSite: 'Strict',    // CSRF protection
+        maxAge: 3600000        // 1 hour in milliseconds
+    });
+  res.status(statusCode).json({
     success: true,
     message,
     token,
